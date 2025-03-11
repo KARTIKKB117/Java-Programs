7b3c323f1eb2875d5fca4d1abf328fd735a5a5be
Kartik  K Bandekar <bandekar.kartik117@gmail.com>
2025-03-11 08:43:14 +0530

    Add files via upload

---
+++ PersonDemo.java
@@ -0,0 +1,25 @@
+        package person;
+
+class Person{
+	 String name;
+	 int age;
+	 
+	 public Person(String name , int age) {
+		 this.name = name ;
+		 this.age = age;
+	 }
+	 
+	 public void display()
+	 {
+		 System.out.println(name+" is "+age+" years old" );
+		 
+			 
+      }
+	  }
+ 
+ public class PersonDemo{
+	 public static void main(String[] args) {
+	 Person p1 = new Person("Vicks", 18);
+	 p1.display();
+ }
+}
---
+++ Rprime.java
@@ -0,0 +1,22 @@
+package prime;
+import java.util.Scanner;
+
+public class Rprime {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Boolean isprime = true;
+		System.out.println("Enter a number:");
+		int n = scanner.nextInt();
+		
+		for(int i=2;i<=n/2;i++) {
+			if (n%i==0)
+				isprime = false;
+		}
+		
+		if(isprime)
+			System.out.println(n+" is  prime ");
+		else
+			System.out.println(n+" is not prime");
+	}
+
+}
