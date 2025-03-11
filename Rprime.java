05d94311832f0b8be801f5810fde706f751b84c2
Kartik  K Bandekar <bandekar.kartik117@gmail.com>
2025-03-11 22:56:51 +0530

    Update Rprime.java

--- Rprime.java
+++ Rprime.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Rprime {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);   //sc
 		Boolean isprime = true;
 		System.out.println("Enter a number:");
-		int n = scanner.nextInt();
+		int n = scanner.nextInt();                  // sc.nextInt();
 		
 		for(int i=2;i<=n/2;i++) {
 			if (n%i==0)
